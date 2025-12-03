496f2bafca27f414d5822eeab401b04dcc0771ed
shane <shaneluo97@foxmail.com>
2025-12-03 20:46:08 +0800

    rm zmq

--- src/sdk/uzmq/uapp/common/global/application.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "uzmq/application.h"
-// #ifdef USE_UZMQ
-// #else // USE_LIBPX
-// #include "px/application.h"
-// #endif
--- src/sdk/uzmq/uapp/common/global/message.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "uzmq/message.h"
-// #ifdef USE_UZMQ
-// #else // USE_LIBPX
-// #include "px/message.h"
-// #endif
